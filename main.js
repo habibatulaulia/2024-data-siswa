f40175438853d3cafd9496cc8ad7bb35880cd844
habibatulaulia76@gmail.com <habibatulaulia76@gmail.com>
2024-01-22 11:14:05 +0700

    menambah konfigurasi firebase

---
+++ main.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { 
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbJDnmlNa2wObKWmQ6MLuXQ5KWbp0FDEA",
+  authDomain: "insan-cemerlang-8011b.firebaseapp.com",
+  projectId: "insan-cemerlang-8011b",
+  storageBucket: "insan-cemerlang-8011b.appspot.com",
+  messagingSenderId: "642542638808",
+  appId: "1:642542638808:web:8dae3c0d85e3be8c5c29ad"
+};
+
+const app = initializeApp(firebaseConfig);
